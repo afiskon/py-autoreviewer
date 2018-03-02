162cd9ea2c9b2724a71ca054c91bfad5b5e3adaf
Aleksander Alekseev <afiskon@gmail.com>
2018-03-02 12:57:52 +0300

    cputube-export.py, now seems to work

--- cputube-export.py
+++ cputube-export.py
@@ -7,6 +7,7 @@ import os
 import re
 
 cputube_url = 'http://commitfest.cputube.org/';
+commitfest_url = 'https://commitfest.postgresql.org/';
 
 headers = {}
 headers['user-agent'] = u'Mozilla/5.0 (compatible; MSIE 9.0; ' + \
@@ -14,21 +15,26 @@ headers['user-agent'] = u'Mozilla/5.0 (compatible; MSIE 9.0; ' + \
 
 body = requests.get(cputube_url, headers = headers).text
 
+print("""
+CREATE TABLE IF NOT EXISTS cputube (url text, apply_passing bool, build_passing bool);
+""");
+sys.stdout.flush()
+
 re_str = "(?is)<tr>\s*(<td>(\d+/\d+)</td>)\s*" + ("(<td[^>]*>(.*?)</td>\s*?)"*5) + "(<td>[^>]+</td>)?\s*</tr>"
 for fi in re.finditer(re_str, body):
     [pid, apply_passing, build_passing] = [fi.group(i) for i in [2, 10, 12]]
+    url = commitfest_url + pid;
     apply_passing = (apply_passing.find("apply-passing.svg") > 0)
-
     if not apply_passing:
         build_passing = False
     else:
         m = re.search('(?i)<img src="(https?://travis-ci.org/[^"]+.svg[^"]+)"', build_passing)
         img_url = m.group(1)
-        print("-- Fetching {}...".format(img_url))
+        # print("-- Fetching {}...".format(img_url))
         img_content = requests.get(img_url, headers = headers).text
         build_passing = (img_content.find(">passing</text>") > 0)
-
-    print("pid: {}".format(pid))
-    print("apply_passing: {}".format(apply_passing))
-    print("build_passing: {}".format(build_passing))
+    print("""
+    INSERT INTO cputube (url, apply_passing, build_passing) VALUES('{}', '{}', '{}');
+    """.format(url, 't' if apply_passing else 'f', 't' if build_passing else 'f'))
+    sys.stdout.flush()
  
